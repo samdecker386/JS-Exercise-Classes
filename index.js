d7ed6ba848442f8418d77ca719083aef8ab6eafd
Sam Decker <samdecker386@gmail.com>
2020-01-30 16:05:52 -0700

    Completed MVP

--- index.js
+++ index.js
@@ -113,10 +113,17 @@ class Lambdasian {
   constructor (attributes) {
     this.name = attributes.name,
     this.age = attributes.age,
-    this.location = attributes.location
+    this.location = attributes.location,
+    this.randomNum = attributes.randomNum
   }
   speak() {
     return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+  gradeStudent(randomNumber) {
+    randomNumber = (Math.random() * (15 - 1) + 1);
+    const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+    randomNumber *= plusOrMinus;
+    this.randomNum = randomNumber;
   }
 }
 
@@ -134,8 +141,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -153,8 +171,35 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+    this.grade = 65;
+  }
+  
+  listSubjects() {
+    return this.favSubjects;
+  }
 
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun spring challenge on ${subject}`;
+  }
+
+  graduate(student) {
+   
+    if (this.randomNum > 70) {
+      return `${student} may graduate!`
+    } else {
+      return `${student} must have more assignments graded to increase score.`
+    }
+  }
 }
 
 /*
@@ -170,8 +215,20 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+   
+  } 
+  standUp (channel, subject) {
+    return `${this.name} debugs ${channel}'s code on ${subject}`;
+  }
+  debugsCode (student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
+  
 }
 
 /*
